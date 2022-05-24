cfafdf0400551fd3d7fb8d36be320d85c3168d7f
chaiqou <lomtadzenikusha@gmail.com>
2022-05-24 22:18:53 +0400

    fix: Update confirmation email link for production

--- resources/views/register/main.blade.php
+++ resources/views/register/main.blade.php
@@ -1,5 +1,4 @@
 <x-layout>
-
     <div class="min-h-full flex">
         <div class="flex-1 flex flex-col justify-center px-4 sm:px-6 lg:px-20 xl:px-28">
             <div class="mx-18 w-full h-full mt-8 max-w-lg lg:w-96">
