2d0027f60986c8bdea97eab58d3f0e387e98bdf3
chaiqou <lomtadzenikusha@gmail.com>
2022-06-03 19:56:33 +0400

    test: Localization test

--- tests/Feature/LocalizationTest.php
+++ tests/Feature/LocalizationTest.php
@@ -2,20 +2,25 @@
 
 namespace Tests\Feature;
 
-use App\Http\Middleware\CheckLocale;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\App;
 use Tests\TestCase;
 
 class LocalizationTest extends TestCase
 {
-	public function test_user_can_change_locale_between_english_and_georgian()
+	public function provideLocales()
 	{
-		$request = Request::create('set-locale/en', 'GET');
-		$request->merge(['locale' => 'en']);
-		$middleware = new CheckLocale();
-		$middleware->handle($request, function ($req) {
-			$this->assertEquals('en', App::getLocale());
-		});
+		return [
+			'english'  => ['en', 'Remember'],
+			'georgian' => ['ka', 'დამიმახსოვრე'],
+		];
+	}
+
+	/**
+	 * @dataProvider provideLocales
+	 */
+	public function testWelcomePageContents($locale)
+	{
+		$this->app->setLocale($locale);
+		$this->get(route('locale.setting', ['locale' => $locale]))
+		->assertStatus(302);
 	}
 }
