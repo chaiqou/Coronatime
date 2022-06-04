3d0c8c0cbfbe09ce5ad58c834b76f52c97012ceb
chaiqou <lomtadzenikusha@gmail.com>
2022-06-04 13:47:32 +0400

    test: Sort Functionality * Finally 100% coverage :)

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -49,4 +49,37 @@ class DashboardTest extends TestCase
 		$response->assertSee('Statistics by country');
 		$response->assertDontSee('Terjola');
 	}
+
+	public function test_sort()
+	{
+		$this->withoutExceptionHandling();
+		$username = 'chaiqou';
+		$password = '123';
+		$is_verified = 1;
+
+		$user = User::factory()->create([
+			'username'    => $username,
+			'password'    => bcrypt(123),
+			'is_verified' => $is_verified,
+		]);
+
+		$response = $this->actingAs($user)->post(route('user.login'), [
+			'username'    => $username,
+			'password'    => 123,
+			'is_verified' => $is_verified,
+		]);
+
+		$this->assertAuthenticated();
+		$response->assertRedirect(route('dashboard.worldwide'));
+
+		Country::factory()->create(['created_at' => '2021-09-01 00:00:00']);
+		Country::factory()->create(['created_at' => '2021-09-02 00:00:00']);
+		$results = Country::orderBy('created_at', 'desc')->get();
+
+		$this->assertCount(2, $results->toArray());
+		$response = $this->get('/by-country?sort=asc&name=name');
+		$response->assertStatus(200);
+		$this->assertEquals('2021-09-02 00:00:00', $results[0]->created_at);
+		$this->assertEquals('2021-09-01 00:00:00', $results[1]->created_at);
+	}
 }
